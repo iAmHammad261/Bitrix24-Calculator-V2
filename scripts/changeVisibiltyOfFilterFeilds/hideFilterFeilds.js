9b6761f79d4ed12ea881227a56f8247b22fce97c
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 17:33:21 +0500

    feat: corrected the filters hiding logic

--- scripts/changeVisibiltyOfFilterFeilds/hideFilterFeilds.js
+++ scripts/changeVisibiltyOfFilterFeilds/hideFilterFeilds.js
@@ -1,6 +1,6 @@
 export const hideFilterFields = (fieldsToHide) => {
 
-    hideFilterFields.forEach(fieldId => {
+    fieldsToHide.forEach(fieldId => {
         const fieldElement = document.getElementById(fieldId);
         if (fieldElement) {
             fieldElement.style.display = 'none';

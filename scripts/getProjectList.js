e01eb1d74f9b0aeaf4474fef65fa901f9027b470
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 11:37:25 +0500

    fiX: added the html and css

---
+++ scripts/getProjectList.js
@@ -0,0 +1,14 @@
+import { callBX24Method } from "./callBX24Method.js"
+
+const getProjectList = async () => {
+
+    try {
+        const projects = await callBX24Method()
+    }
+    catch (error){
+        
+    }
+
+
+
+}

fb2709e5b19057ca13756f19719488382dc0fdd8
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 16:41:58 +0500

    feat: added the console.log to debug for the product data

--- scripts/changeFields.js/changeTheItemFeilds.js
+++ scripts/changeFields.js/changeTheItemFeilds.js
@@ -6,6 +6,8 @@ export const changeTheItemFields = async (productID) => {
 
     const productData = await callBX24Method('catalog.product.get', {id: productID});
 
+    console.log("Fetched product data:", productData);
+
     const baseRate = productData.property115;
     const grossarea = productData.property113;
 

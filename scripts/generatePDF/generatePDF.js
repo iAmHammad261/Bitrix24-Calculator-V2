3bf89b58f962cbffbe1d41156f281be7feb4b6e6
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 21:24:34 +0500

    fix: change how the project is gotton

--- scripts/generatePDF/generatePDF.js
+++ scripts/generatePDF/generatePDF.js
@@ -3,14 +3,14 @@ export const generatePDFOfSummary = () => {
     const doc = new jsPDF();
 
     //  add the heading to the PDF
-    doc.setFontSize(22);
+    doc.setFontSize(18);
     doc.text("Premier Choice",20,20)
 
     // get the project field
-    const projectField = document.getElementById("project-name");
+    const projectFieldValue = document.getElementById("project-name").value;
 
     // add the next heading in the same row
-    doc.text(`{projectField.value}`, 150, 20);
+    doc.text(`${projectFieldValue}`, 150, 20);
 
 
 

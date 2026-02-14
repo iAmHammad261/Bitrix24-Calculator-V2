8a5f902578d288ec04a1d31a60ac10b37b67951e
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 13:38:39 +0500

    feat: added the change for the project, category and type

--- main.js
+++ main.js
@@ -9,9 +9,29 @@ await populateFilters();
 // Select DOM elements
 const button = document.getElementById('myButton');
 const outputText = document.getElementById('output-text');
+const projectSelect = document.getElementById('project-name');
+const propertyTypeSelect = document.getElementById('property-type');
+const propertyCategorySelect = document.getElementById('property-category');
+
 
 // Add an event listener
 button.addEventListener('click', () => {
-    outputText.textContent = "You clicked the button! JavaScript is working.";
+    // outputText.textContent = "You clicked the button! JavaScript is working.";
     console.log("Button clicked.");
 });
+
+projectSelect.addEventListener('change', () => {
+    // outputText.textContent = `Selected project: ${projectSelect.value}`;
+    console.log("Project selected:", projectSelect.value);
+});
+
+propertyTypeSelect.addEventListener('change', () => {
+    // outputText.textContent = `Selected property type: ${propertyTypeSelect.value}`;
+    console.log("Property type selected:", propertyTypeSelect.value);
+})
+
+propertyCategorySelect.addEventListener('change', () => {
+    // outputText.textContent = `Selected property category: ${propertyCategorySelect.value}`;
+    console.log("Property category selected:", propertyCategorySelect.value);
+});
+

4ec3a92bb30880cfc067cd388f276fe1bb834505
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-14 21:20:39 +0500

    feat: added the project name to the document

--- scripts/generatePDF/generatePDF.js
+++ scripts/generatePDF/generatePDF.js
@@ -2,7 +2,17 @@ export const generatePDFOfSummary = () => {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
+    //  add the heading to the PDF
+    doc.setFontSize(22);
     doc.text("Premier Choice",20,20)
+
+    // get the project field
+    const projectField = document.getElementById("project-name");
+
+    // add the next heading in the same row
+    doc.text(`{projectField.value}`, 150, 20);
+
+
 
 
     doc.save("summary.pdf")

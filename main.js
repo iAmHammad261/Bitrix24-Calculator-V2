433fbf126f28beb56d31168e6e8bd5020759a023
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-15 16:36:17 +0500

    feat: added the blob function

--- main.js
+++ main.js
@@ -11,132 +11,125 @@ import { getPlacementInfo } from "./scripts/Bitrix24HelperFunctions/getPlacement
 import { attachFileToLead } from "./scripts/attachFileToLead/attachFileToLead.js";
 
 // A simple console log to verify connection
-console.log('Script loaded successfully from the scripts folder!');
+console.log("Script loaded successfully from the scripts folder!");
 
 await populateFilters();
 
-
 // Select DOM elements
-const button = document.getElementById('myButton');
-const outputText = document.getElementById('output-text');
-const projectSelect = document.getElementById('project-name');
-const propertyTypeSelect = document.getElementById('property-type');
-const propertyCategorySelect = document.getElementById('property-category');
+const button = document.getElementById("myButton");
+const outputText = document.getElementById("output-text");
+const projectSelect = document.getElementById("project-name");
+const propertyTypeSelect = document.getElementById("property-type");
+const propertyCategorySelect = document.getElementById("property-category");
 const itemFilterSelect = document.getElementById("property-item");
 const paymentMethodSelect = document.getElementById("payment-condition");
-const downPaymentPercentageSelect = document.getElementById("downpayment-percentage");
-const onPossessionPercentageSelect = document.getElementById("possession-percentage");
+const downPaymentPercentageSelect = document.getElementById(
+  "downpayment-percentage",
+);
+const onPossessionPercentageSelect = document.getElementById(
+  "possession-percentage",
+);
 const installmentPlanSelect = document.getElementById("installment-duration");
 const downloadButtonSelect = document.getElementById("menu-download-pdf");
 const attachPDFButtonSelect = document.getElementById("menu-attach-lead");
 
 const handleFilterChange = async () => {
-    const filters = {
-        project: projectSelect.value,
-        propertyType: propertyTypeSelect.value,
-        propertyCategory: propertyCategorySelect.value
-    }
+  const filters = {
+    project: projectSelect.value,
+    propertyType: propertyTypeSelect.value,
+    propertyCategory: propertyCategorySelect.value,
+  };
 
-    console.log("Current filters:", filters);
+  console.log("Current filters:", filters);
 
-    if(filters){
-        const productList = await getTheProductWithFilter(filters);
-            populateItemFilter(productList);
-    }
-
-}
+  if (filters) {
+    const productList = await getTheProductWithFilter(filters);
+    populateItemFilter(productList);
+  }
+};
 
 const handleItemChange = async () => {
-    downloadButtonSelect.disabled = false;
-    const selectedItemId = itemFilterSelect.value;
-    await changeTheItemFields(selectedItemId);
-    changeTheFinanceFields();
-    createTableOfInstallments();
-}
+  downloadButtonSelect.disabled = false;
+  const selectedItemId = itemFilterSelect.value;
+  await changeTheItemFields(selectedItemId);
+  changeTheFinanceFields();
+  createTableOfInstallments();
+};
 
 const handlePaymentMethodChange = () => {
+  const selectedPaymentMethod = paymentMethodSelect.value;
 
-    const selectedPaymentMethod = paymentMethodSelect.value;
+  console.log("Selected payment method:", selectedPaymentMethod);
 
-    console.log("Selected payment method:", selectedPaymentMethod);
-
-    if( selectedPaymentMethod == "full"){
-        hideFilterFields(["installment-options-container"])
-        changeTheFinanceFields();
-        createTableOfInstallments();
-    }
-    if(selectedPaymentMethod == "installment"){
-        unhideFilterFields(["installment-options-container"])
-         changeTheFinanceFields();
-        createTableOfInstallments();
-    }
-
-}
-
+  if (selectedPaymentMethod == "full") {
+    hideFilterFields(["installment-options-container"]);
+    changeTheFinanceFields();
+    createTableOfInstallments();
+  }
+  if (selectedPaymentMethod == "installment") {
+    unhideFilterFields(["installment-options-container"]);
+    changeTheFinanceFields();
+    createTableOfInstallments();
+  }
+};
 
 // handle the change of the downpayment percentage,on possession percentage, and installment plans
 const handlechangeOfFinanceValues = () => {
-   changeTheFinanceFields();
-   createTableOfInstallments();
-}
+  changeTheFinanceFields();
+  createTableOfInstallments();
+};
 
 const downloadPDFSummary = async () => {
-    const pdfDoc = await generatePDFOfSummary();
-    pdfDoc.save("summary.pdf");
-}
+  const pdfDoc = await generatePDFOfSummary();
+  pdfDoc.save("summary.pdf");
+};
 
 const attachPDFToLead = async () => {
+  console.log("[Attach PDF] Starting process to attach PDF to Lead...");
 
-    console.log("[Attach PDF] Starting process to attach PDF to Lead...");
+  const leadID = getPlacementInfo()["options"]["ID"];
 
-    const leadID = getPlacementInfo()['options']['ID'];
+  const file = await generatePDFOfSummary();
 
-    const file = await generatePDFOfSummary();
+  const pdfBlob = file.output("blob");
 
-    attachFileToLead(leadID, file);
+  pdfBlob.name = `Payment-Plan-${leadID}.pdf`;
 
-    console.log(`[Attach PDF] Retrieved Lead ID from placement info: ${leadID}`);   
+  attachFileToLead(leadID, pdfBlob);
 
-    
+  console.log(`[Attach PDF] Retrieved Lead ID from placement info: ${leadID}`);
 
-    // const pdfDoc = await generatePDFOfSummary();
+  // const pdfDoc = await generatePDFOfSummary();
 
-    // // convert to base64 String
-    // const fullDataUri = await pdfDoc.output('datauristring');
-    // const base64String = fullDataUri.split(',')[1]; 
-    
-    // await attachFileToLead(leadId, base64String);
+  // // convert to base64 String
+  // const fullDataUri = await pdfDoc.output('datauristring');
+  // const base64String = fullDataUri.split(',')[1];
 
-}
+  // await attachFileToLead(leadId, base64String);
+};
 
+projectSelect.addEventListener("change", handleFilterChange);
 
+propertyTypeSelect.addEventListener("change", handleFilterChange);
 
-projectSelect.addEventListener('change', handleFilterChange);
+propertyCategorySelect.addEventListener("change", handleFilterChange);
 
-propertyTypeSelect.addEventListener('change', handleFilterChange)
+itemFilterSelect.addEventListener("change", handleItemChange);
 
-propertyCategorySelect.addEventListener('change', handleFilterChange);
+paymentMethodSelect.addEventListener("change", handlePaymentMethodChange);
 
-itemFilterSelect.addEventListener('change', handleItemChange);
+downPaymentPercentageSelect.addEventListener(
+  "change",
+  handlechangeOfFinanceValues,
+);
 
-paymentMethodSelect.addEventListener('change', handlePaymentMethodChange);
+onPossessionPercentageSelect.addEventListener(
+  "change",
+  handlechangeOfFinanceValues,
+);
 
-downPaymentPercentageSelect.addEventListener('change', handlechangeOfFinanceValues);
+installmentPlanSelect.addEventListener("change", handlechangeOfFinanceValues);
 
-onPossessionPercentageSelect.addEventListener('change', handlechangeOfFinanceValues);
+downloadButtonSelect.addEventListener("click", downloadPDFSummary);
 
-installmentPlanSelect.addEventListener('change', handlechangeOfFinanceValues);
-
-downloadButtonSelect.addEventListener('click', downloadPDFSummary);
-
-attachPDFButtonSelect.addEventListener('click', attachPDFToLead);
-
-
-
-
-
-
-
-
-
-
+attachPDFButtonSelect.addEventListener("click", attachPDFToLead);

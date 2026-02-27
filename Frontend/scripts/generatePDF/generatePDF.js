433afc9ccdb59cccd2ed8719243dbb74208384be
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2026-02-27 12:53:29 +0500

    feat: changed the box park 3 logo

--- Frontend/scripts/generatePDF/generatePDF.js
+++ Frontend/scripts/generatePDF/generatePDF.js
@@ -226,13 +226,13 @@ const currentCalculations = {
           "https://i.postimg.cc/50nFTm0P/DHA-Orchard-Night-Shot-01-Small.jpg",
       },
       "Box Park-3":{
-        logoUrl: 'https://images.premierchoiceint.online/image/Z1h',
-        imageUrl: 'https://images.premierchoiceint.online/image/jL'
+        logoUrl: 'https://images.premierchoiceint.online/images/2026/02/27/sales-offer-Box-Park-3-04.png',
+        imageUrl: 'https://images.premierchoiceint.online/images/2026/02/27/Sales-Offer-Box-Park-III.jpgcb69ff77704270aa.jpeg'
       },
       default: {
         logoUrl: "https://i.postimg.cc/SxkkYbV8/Grand-Orchard.png",
         imageUrl:
-          "https://images.premierchoiceint.online/images/2026/02/27/Sales-Offer-Box-Park-III.jpgcb69ff77704270aa.jpeg",
+          "https://i.postimg.cc/50nFTm0P/DHA-Orchard-Night-Shot-01-Small.jpg",
       },
     };
 
@@ -256,16 +256,16 @@ const currentCalculations = {
     console.log("[PDF Gen] Drawing Page 1...");
 
 
-   if(projectName == 'Box Park-3'){
-      const firstPageImageURL = "https://images.premierchoiceint.online/images/2026/02/27/Sales-Offer-Box-Park-III.jpgcb69ff77704270aa.jpeg"
-      try {
-        const base64OfFirstImage = await imageToBase64(firstPageImageURL);
-        doc.addImage(base64OfFirstImage, "JPEG", 0, 0, pageW, pageH);
-        doc.addPage();
-      } catch(e) {
-        console.warn("[PDF Gen] Skipping Box Park-3 image due to CORS/Load error");
-      }
-    }
+  //  if(projectName == 'Box Park-3'){
+  //     const firstPageImageURL = "https://images.premierchoiceint.online/images/2026/02/27/Sales-Offer-Box-Park-III.jpgcb69ff77704270aa.jpeg"
+  //     try {
+  //       const base64OfFirstImage = await imageToBase64(firstPageImageURL);
+  //       doc.addImage(base64OfFirstImage, "JPEG", 0, 0, pageW, pageH);
+  //       doc.addPage();
+  //     } catch(e) {
+  //       console.warn("[PDF Gen] Skipping Box Park-3 image due to CORS/Load error");
+  //     }
+  //   }
 
     // --- PAGE 1: TITLE PAGE ---
     let currentY = 15;
